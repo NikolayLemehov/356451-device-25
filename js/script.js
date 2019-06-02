7a33f0180cb5cd3b7ee7bd749f01441850cb4723
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-06-02 20:46:45 +0300

    Подключение js. Анимация появления карты и формы обратной связи.

---
+++ js/script.js
@@ -0,0 +1,95 @@
+var currentLocation = window.location;
+
+function writeUs() {
+  var openPopupButton = document.querySelector(".contacts__btn");
+  var popup = document.querySelector(".modal.write-us");
+  var closePopupButton = popup.querySelector(".btn-modal-close");
+  var form = popup.querySelector("form");
+  var nameField = popup.querySelector("[name=name]");
+  var emailField = popup.querySelector("[name=email]");
+  var textField = popup.querySelector("[name=text]");
+  var isStorageSupport = true;
+  var nameStorage = "";
+  var emailStorage = "";
+
+  try {
+    nameStorage = localStorage.getItem("nameField");
+    emailStorage = localStorage.getItem("emailField");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+  openPopupButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal__show");
+    if (nameStorage) {
+      nameField.value = nameStorage;
+    } else {
+      nameField.focus();
+    }
+    if (emailStorage) {
+      emailField.value = emailStorage;
+    }
+    if (nameStorage && !emailStorage) {
+      emailField.focus();
+    }
+    if (nameStorage && emailStorage) {
+      textField.focus();
+    }
+  });
+  closePopupButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal__show");
+    popup.classList.remove("modal-error");
+  });
+  form.addEventListener("submit", function (evt) {
+    if (!nameField.value || !emailField.value || !textField.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      void popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("nameField", nameField.value);
+        localStorage.setItem("emailField", emailField.value);
+      }
+    }
+  });
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal__show")) {
+        popup.classList.remove("modal__show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
+}
+
+function map() {
+  var openMap = document.querySelector(".contacts__map");
+  var popup = document.querySelector(".modal-map");
+  var closePopupButton = popup.querySelector(".btn-modal-close");
+
+  openMap.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal__show");
+  });
+  closePopupButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal__show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal__show")) {
+        popup.classList.remove("modal__show");
+      }
+    }
+  });
+}
+
+if (currentLocation.pathname.lastIndexOf("/index.html") >= 0) {
+  writeUs();
+  map();
+}
